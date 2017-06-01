ca5f02d7375f2113180c96afe60b9749eabf6711
danielkraus <krausdaniel@me.com>
2017-06-01 13:57:32 +0200

    styles for sorting dropdown

--- components/iconArrow.js
+++ components/iconArrow.js
@@ -1,11 +1,7 @@
 export default () => (
   <svg width="15px" height="15px" viewBox="0 0 15 15" version="1.1" xmlns="http://www.w3.org/2000/svg">
-    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-      <g fill="#444444">
-        <g transform="translate(7.500000, 7.500000) scale(-1, -1) rotate(90.000000) translate(-7.500000, -7.500000) ">
-          <polygon  transform="translate(7.500000, 7.375000) scale(-1, 1) translate(-7.500000, -7.375000) " points="7.49999958 0.375 6.26624974 1.60874979 11.1487494 6.49999963 0.5 6.49999963 0.5 8.24999953 11.1487494 8.24999953 6.26624974 13.1412493 7.49999958 14.3749992 14.4999992 7.37499958"></polygon>
-        </g>
-      </g>
+    <g fill="#444444" fill-rule="evenodd">
+      <polygon transform="translate(7.500000, 7.500000) scale(-1, 1) translate(-7.500000, -7.500000) " points="7.5 8.8817842e-16 6.17812509 1.32187485 11.409375 6.5625 8.8817842e-16 6.5625 8.8817842e-16 8.4375 11.409375 8.4375 6.17812509 13.6781251 7.5 15 15 7.5"></polygon>
     </g>
   </svg>
 )
--- components/sortby.js
+++ components/sortby.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import Check from './iconCheck'
+import Arrow from './iconArrow'
 
 export default class extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      isOpen: false
+      isOpen: false,
+      selectedItem: 'Date Created',
     }
     this.handleClick = this.handleClick.bind(this)
   }
@@ -15,31 +18,96 @@ export default class extends Component {
 
   render () {
     const { list, children, activeItem } = this.props
-    const { isOpen } = this.state
+    const { isOpen, selectedItem } = this.state
     return (
-      <div>
+      <div className="wrapper">
         <button onClick={this.handleClick}>
           Sort By
+          <span className="button__selected">
+            {selectedItem}
+          </span>
+          <span className={`arrow ${isOpen && 'arrow--open'}`}>
+            <Arrow />
+          </span>
         </button>
         {isOpen && (
           <ul>
-            {list.map(item => (
-              <li>
-                <button className="item">{item}</button>
-              </li>
-            ))}
+            <li className={'selected'}><Check /> Date Created</li>
+            <li>Priority</li>
+            <li>Completed</li>
+            <li>Title</li>  
           </ul>
         )}
         <style jsx>
           {`
+          .wrapper {
+            position:relative;
+          }
           button {
-            font-size: 1rem;
+            font-size: 1.5rem;
             border: none;
             background: none;
             cursor: pointer;
+            font-weight: 600;
+            outline: 0;
+            padding-right: 15px;
+            color: #444444;
           }
+
+          .button__selected {
+            font-weight: 400;
+            margin: 0 10px;
+            min-width: 90px;
+            display: inline-block;
+          }
+
+          .arrow {
+            position: absolute;
+            width: 15px;
+            height: 15px;
+            top: 4px;
+            right: 0;
+            display: inline-block;
+            transition: transform .2s ease;
+            transform: rotate(-90deg);
+          }
+
+          .arrow--open {
+            transform: rotate(90deg);
+          }
+
           ul {
-            
+            list-style: none;
+            margin: 0;
+            width: 100%;
+            background: #fff;
+            box-shadow: 0 20px 70px 0 #E7E2EF;
+            padding: 24px 22px;
+            position: absolute;
+            top: 52px;
+          }
+
+          li {
+            padding: 0 0 0 30px; 
+            margin: 5px 0;
+            position: relative;
+            cursor: pointer;
+            font-size: 1.5rem;
+            opacity: 0.5;
+            transition: opacity .15s ease;
+          }
+
+          li:hover {
+            opacity: 1;
+          }
+
+          li.selected{
+            opacity: 1;
+          }
+
+          li > :global(.icon) {
+            position: absolute;
+            left: 0;
           }
         `}
         </style>
