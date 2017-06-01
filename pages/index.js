aa8e3f8767cc3ea549995ba0e6fd9b669d29bd24
Pavel Prichodko <prichodko.p@gmail.com>
2017-06-01 16:52:54 +0200

    Implement Rapid to index page

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,44 @@
+import React, { Component } from 'react'
+
 import Page from '../components/page'
 import Header from '../components/header'
 import TodoList from '../components/todolist'
+import Edit from '../components/edit'
 
-export default () => (
-  <Page heading="Tasks">
-    <TodoList todos={todos}/>
-  </Page>
-)
+import {observeTodos} from '../lib/observe-todos'
+import updateTodo from '../lib/update-todo'
 
-
-const todos = [
-  {
-    id: 1,
-    priority: 'low',
-    completed: false,
-    tag: 'home',
-    title: 'hello',
-    description: 'Some descriptioon'
-  },
-  {
-    id: 2,
-    priority: 'high',    
-    completed: false,
-    tag: 'work',
-    title: 'hello',
-    description: 'Some descriptioon'
-  },
-  {
-    id: 3,
-    priority: 'medium',
-    completed: false,
-    tag: 'other',
-    title: 'hello',
-    description: 'Some descriptioon'
+export default class extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: [],
+      editing: null,
+    }
   }
-]
+  
+  componentDidMount() {
+    this.todos = observeTodos(todos => this.setState({ todos }))
+  }
+
+  componentWillUnmount() {
+    this.todos.unsubscribe()
+  }
+
+  handleCheckboxClick = async (id, completed) => {
+    await updateTodo(id, { completed: !completed })
+  }
+
+  handleTodoClick = id => {
+    this.setState({ editing: id, })
+  }
+  
+  render() {
+    const { todos, editing } = this.state
+    return (
+      <Page heading="Tasks">
+        <TodoList todos={todos} onCheckboxClick={this.handleCheckboxClick} onTodoClick={this.handleTodoClick} />
+      </Page>
+    )
+  }
+}
