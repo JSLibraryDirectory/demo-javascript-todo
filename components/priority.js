18f7e82fb396d1de9e3f486cf6df72368a3a0c71
Pavel Prichodko <prichodko.p@gmail.com>
2017-06-01 12:53:20 +0200

    add priority

---
+++ components/priority.js
@@ -0,0 +1,35 @@
+const constants = {
+  'low': 1,
+  'medium': 2,
+  'high': 3
+}
+
+const Dot = () => (
+  <span>
+    <style jsx>{`
+      span {
+        height: 8px;
+        width: 8px;
+        border-radius: 50%;
+        background: black;
+        margin: 2px;
+        display: inline-block;
+      }
+    `}
+    </style>
+  </span>
+)
+
+export default ({ priority }) => (
+  <div>
+    {[...Array(constants[priority]).keys()].map(() => <Dot />)}
+    <style jsx>{`
+      div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+      }
+    `}
+    </style>
+  </div>
+)
--- pages/index.js
+++ pages/index.js
@@ -12,18 +12,21 @@ export default () => (
 const todos = [
   {
     id: 1,
+    priority: 'low',
     completed: false,
     title: 'hello',
     description: 'Some descriptioon'
   },
   {
     id: 2,
+    priority: 'high',    
     completed: false,
     title: 'hello',
     description: 'Some descriptioon'
   },
   {
     id: 3,
+    priority: 'medium',
     completed: false,
     title: 'hello',
     description: 'Some descriptioon'
