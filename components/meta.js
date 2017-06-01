562d415a7fae47c5c68ee862685ebbd2e4d973ff
danielkraus <krausdaniel@me.com>
2017-06-02 00:07:42 +0200

    add meta tags

--- components/meta.js
+++ components/meta.js
@@ -3,6 +3,8 @@ import Head from 'next/head'
 export default () => (
   <div>
     <Head>
+      <title>Rapid.io Demo App</title>
+      <meta name="description" content="Rapid.io is a flexible realtime database accessible from a client-side code. Go realtime using rich queries, auto indexing and flexible security. Download our SDKs for iOS, Android and JavaScript on www.rapid.io. Start for free." />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
       <link rel="shortcut icon" href="/static/favicon.ico" />
