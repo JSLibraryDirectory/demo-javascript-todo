8319fa40904f74700de2337f62c9afafbc503d3e
Pavel Prichodko <prichodko.p@gmail.com>
2017-06-01 22:33:41 +0200

    Change todo state locally

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,18 @@ import React, { Component } from 'react'
 import Page from '../components/page'
 import Header from '../components/header'
 import TodoList from '../components/todolist'
-import Edit from '../components/edit'
 
-import {observeTodos} from '../lib/observe-todos'
+import { observeTodos } from '../lib/observe-todos'
 import updateTodo from '../lib/update-todo'
 
 export default class extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      todos: [],
-      editing: null,
+      todos: []
     }
   }
-  
+
   componentDidMount() {
     this.todos = observeTodos(todos => this.setState({ todos }))
   }
@@ -26,18 +24,22 @@ export default class extends Component {
   }
 
   handleCheckboxClick = async (id, completed) => {
+    this.setState(prev => ({
+      todos: prev.todos.map(todo => {
+        todo.body = todo.id === id
+          ? { ...todo.body, completed: !completed }
+          : todo.body
+        return todo
+      })
+    }))
     await updateTodo(id, { completed: !completed })
   }
 
-  handleTodoClick = id => {
-    this.setState({ editing: id, })
-  }
-  
   render() {
-    const { todos, editing } = this.state
+    const { todos } = this.state
     return (
       <Page heading="Tasks">
-        <TodoList todos={todos} onCheckboxClick={this.handleCheckboxClick} onTodoClick={this.handleTodoClick} />
+        <TodoList todos={todos} onCheckboxClick={this.handleCheckboxClick} />
       </Page>
     )
   }
